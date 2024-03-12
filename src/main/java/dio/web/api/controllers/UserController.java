ae3ff8181d701f27ed0be22d9ba0878513688264
bbrunabrito <brunabritoalves@hotmail.com>
2024-03-12 16:58:08 -0300

    Create repository

---
+++ src/main/java/dio/web/api/controllers/UserController.java
@@ -0,0 +1,33 @@
+package dio.web.api.controllers;
+
+import dio.web.api.model.Usuario;
+import dio.web.api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserRepository repository;
+
+    @GetMapping()
+    public List<Usuario> getUsers() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/{username}")
+    public Usuario getOne(@PathVariable("username") String username) {
+        return repository.findByUsername(username);
+    }
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") Integer id){
+        repository.deleteById(id);
+    }
+    @PostMapping()
+    public void postUser(@RequestBody Usuario user){
+        repository.save(user);
+    }
+}
---
+++ src/main/java/dio/web/api/controllers/WelcomeController.java
@@ -0,0 +1,12 @@
+package dio.web.api.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping
+    public String welcome() {
+        return "Welcome to the Digital Innovation One!";
+    }
+}
